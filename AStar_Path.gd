fc8bc4650917d7bf38780846ee312ac5876dc014
Allen Schmidt <cobrasoft@gmail.com>
2022-04-02 06:10:56 -0400

    Initial commit - Let's Jam!

---
+++ AStar_Path.gd
@@ -0,0 +1,45 @@
+extends TileMap
+class_name AStar_Path
+
+# Most of this code is from AndOne's YouTube video "A* Path-Finding for Grid-Based Tilemap in Godot
+# https://www.youtube.com/watch?v=dVNH6mIDksQ
+
+onready var astar = AStar2D.new()
+onready var used_cells = get_used_cells()
+
+var path : PoolVector2Array
+
+
+func _ready():
+	_add_points()
+	_connect_points()
+
+
+func _add_points():
+	for cell in used_cells:
+		var cell_type = get_cellv(cell)
+		print('Cell: ', cell_type, cell)
+		astar.add_point(id(cell), cell, 1.0)
+
+
+func _connect_points():
+	for cell in used_cells:
+		for neighbor in [Vector2.UP, Vector2.LEFT, Vector2.DOWN, Vector2.RIGHT]:
+			var next_cell = cell + neighbor
+			if used_cells.has(next_cell):
+				# Change here to implement one-way streets
+				astar.connect_points(id(cell), id(next_cell), false)
+
+
+func _get_path(start, end):
+	path = astar.get_point_path(id(start), id(end))
+	# Remove the first point because we are already there
+	path.remove(0)
+
+
+# Cantor pairing function
+func id(point : Vector2):
+	var a = point.x
+	var b = point.y
+	return (a + b) * (a + b + 1) / 2 + b
+
---
+++ Road.gd
@@ -0,0 +1,43 @@
+extends AStar_Path
+
+# Most of this code is from AndOne's YouTube video "A* Path-Finding for Grid-Based Tilemap in Godot
+# https://www.youtube.com/watch?v=dVNH6mIDksQ
+
+onready var car = $Car
+
+
+var car_moving = false
+var car_offset = Vector2(8, 0)
+var tile_center = Vector2(8, 8)
+
+
+func _input(event):
+	if (event is InputEventMouseButton and event.is_pressed() and event.button_index == BUTTON_LEFT) and not car_moving:
+		var mouse_position = world_to_map(get_global_mouse_position())
+		if used_cells.has(mouse_position):
+			var car_position = world_to_map(car.global_position)
+			_get_path(car_position, mouse_position)
+			car_moving = true
+			move()
+	
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == BUTTON_RIGHT:
+		pass
+
+
+func move():
+	for point in path:
+		var next_position = map_to_world(point)
+		var car_direction = (next_position - car.global_position).normalized()
+		match car_direction:
+			Vector2.UP:
+				car.frame = 0
+			Vector2.DOWN:
+				car.frame = 3
+			Vector2.LEFT:
+				car.frame = 2
+			Vector2.RIGHT:
+				car.frame = 1
+		car.global_position = next_position + tile_center + car_offset.rotated(car_direction.angle())
+		yield(get_tree().create_timer(0.1), "timeout")
+	
+	car_moving = false
